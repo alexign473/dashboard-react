15e5c363b8f5c5e6681f7cb0aea12786fac99e83
Alexander Ignatev <alexign473@gmail.com>
2022-06-13 08:14:14 +0300

    add kanban, editor, color picker apps

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from '@syncfusion/ej2-react-richtexteditor';
+
+// project imports
+import { EditorData } from '../data/dummy';
+import { Header } from '../components';
+import { Layout } from '.';
 
 export const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <Layout>
+      <Header category='App' title='Editor' />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </Layout>
+  );
 };
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { KanbanComponent, ColumnDirective, ColumnsDirective } from '@syncfusion/ej2-react-kanban';
+
+// project imports
+import { kanbanData, kanbanGrid } from '../data/dummy';
+import { Header } from '../components';
+import { Layout } from '.';
 
 export const Kanban = () => {
-  return <div>Kanban</div>;
+  return (
+    <Layout>
+      <Header category='App' title='Kanban' />
+      <KanbanComponent
+        id='kanban'
+        dataSource={kanbanData}
+        cardSettings={{
+          contentField: 'Summary',
+          headerField: 'Id',
+        }}
+        keyField='Status'
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, i) => (
+            <ColumnDirective key={i} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </Layout>
+  );
 };
